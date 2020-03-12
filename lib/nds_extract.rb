c60962354be86a68d687b14e7e0fd33d65162d68
Paul Ralyea <047par@gmail.com>
2020-03-12 18:26:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,8 +25,16 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
-end
+  director_index = 0
+   director_list = Array.new
+
+   while director_index < source.size do
+     director_list.push(source[director_index][:name])
+     director_index += 1
+   end
+   director_list
+ end
+
 
 def total_gross(source)
   # Write this implementation
